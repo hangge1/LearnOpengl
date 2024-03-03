2dd4688a104c9fa265bbf6bcab5f8c03c284cc05
hangdada <824362330@qq.com>
2024-03-03 16:18:42 +0800

    你好，三角形的课后练习题1

--- StartOpenGL/src/Application.cpp
+++ StartOpenGL/src/Application.cpp
@@ -128,23 +128,20 @@ int main()
         return -4;
     }
 
-    /*GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
-    };*/
-
     GLfloat vertices[] = {
-    0.5f, 0.5f, 0.0f,   // 右上角
-    0.5f, -0.5f, 0.0f,  // 右下角
-    -0.5f, -0.5f, 0.0f, // 左下角
-    -0.5f, 0.5f, 0.0f   // 左上角
+        -0.5f,  0.0f, 0.0f,
+         0.0f,  0.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f,
+
+         0.0f,  0.0f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
     };
 
-    GLuint indices[] = { // 注意索引从0开始! 
-        0, 1, 3, // 第一个三角形
-        1, 2, 3  // 第二个三角形
-    };
+    //GLuint indices[] = { // 注意索引从0开始! 
+    //    0, 1, 3, // 第一个三角形
+    //    1, 2, 3  // 第二个三角形
+    //};
 
     //初始化VBO VAO 
     GLuint VAO;
@@ -156,10 +153,10 @@ int main()
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
         //3. EBO
-        GLuint EBO;
+        /*GLuint EBO;
         glGenBuffers(1, &EBO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
         //4. 设置顶点属性指针
             //(1)属性index
             //(2)count
@@ -183,14 +180,13 @@ int main()
         //渲染三角形
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         // 交换缓冲
         glfwSwapBuffers(window);
     }
-
 
     glfwTerminate();
     return 0;
