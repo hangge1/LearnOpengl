0d4166c20a005e2c42a318009a4765523363cacf
hangdada <824362330@qq.com>
2024-03-03 14:26:04 +0800

    引入EBO，绘制两个三角形形成一个矩形

--- StartOpenGL/src/Application.cpp
+++ StartOpenGL/src/Application.cpp
@@ -128,13 +128,24 @@ int main()
         return -4;
     }
 
-    GLfloat vertices[] = {
+    /*GLfloat vertices[] = {
         -0.5f, -0.5f, 0.0f,
          0.5f, -0.5f, 0.0f,
          0.0f,  0.5f, 0.0f
+    };*/
+
+    GLfloat vertices[] = {
+    0.5f, 0.5f, 0.0f,   // 右上角
+    0.5f, -0.5f, 0.0f,  // 右下角
+    -0.5f, -0.5f, 0.0f, // 左下角
+    -0.5f, 0.5f, 0.0f   // 左上角
     };
 
-    
+    GLuint indices[] = { // 注意索引从0开始! 
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+
     //初始化VBO VAO 
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
@@ -144,7 +155,12 @@ int main()
         // 2. 把顶点数组复制到缓冲中供OpenGL使用
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-        // 3. 设置顶点属性指针
+        //3. EBO
+        GLuint EBO;
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+        //4. 设置顶点属性指针
             //(1)属性index
             //(2)count
             //(3)数据基本类型
@@ -152,7 +168,7 @@ int main()
             //(5)当前属性的起始偏移
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
         glEnableVertexAttribArray(0);
-        //4. 解绑VAO
+    //4. 解绑VAO
     glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window))
@@ -167,7 +183,8 @@ int main()
         //渲染三角形
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glBindVertexArray(0);
 
         // 交换缓冲
