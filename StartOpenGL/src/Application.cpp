1afc4931ebe9616828ed991ac2178e95260e08eb
hangdada <824362330@qq.com>
2024-03-03 18:39:44 +0800

    着色器练习题1，修改着色器使得三角形上下颠倒

--- StartOpenGL/src/Application.cpp
+++ StartOpenGL/src/Application.cpp
@@ -48,15 +48,15 @@ int main()
 
     GLfloat vertices[] = {
          0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // 右上角
-         0.5f,-0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // 右下角
-        -0.5f,-0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // 左下角
+         0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, // 右下角
+        -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, // 左下角
         -0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f  // 左上角
     };
 
-    //GLuint indices[] = { // 注意索引从0开始! 
-    //    0, 1, 3, // 第一个三角形
-    //    1, 2, 3  // 第二个三角形
-    //};
+    GLuint indices[] = { // 注意索引从0开始! 
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
 
 
     GLuint VAO;
@@ -66,12 +66,12 @@ int main()
         glGenBuffers(1, &VBO);
         // 2. 把顶点数组复制到缓冲中供OpenGL使用
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices) / 2, vertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
         //3. EBO
-        /*GLuint EBO;
+        GLuint EBO;
         glGenBuffers(1, &EBO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
         //4. 设置顶点属性指针
             //(1)属性index
             //(2)count
